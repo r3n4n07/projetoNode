f157d3d37263a6ecec1974facecd816c3d990eff
Renan Augusto Silva Pereira <renan07102002@gmail.com>
2024-09-19 18:05:26 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log();
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("HELLO WORD");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
