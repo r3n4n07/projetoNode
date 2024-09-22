c339310713720b7dbd6a0a0ce63f32a7f1f1f25b
Renan Augusto Silva Pereira <renan07102002@gmail.com>
2024-09-21 23:21:35 -0300

    feat: add MongoUser protocol

--- src/repositories/update-user/mongo-update-user.ts
+++ src/repositories/update-user/mongo-update-user.ts
@@ -5,6 +5,7 @@ import {
 } from "@/controllers/update-user/protocols";
 import { MongoClient } from "@/database/mongo";
 import { User } from "@/models/user";
+import { MongoUser } from "../mongo-protocols";
 
 export class MongoUpdateUserRepository implements IUpdateUserRepository {
   async updateUser(id: string, params: UpdateUserParams): Promise<User> {
@@ -13,7 +14,7 @@ export class MongoUpdateUserRepository implements IUpdateUserRepository {
       .updateOne({ _id: new ObjectId(id) }, { $set: { ...params } });
 
     const user = await MongoClient.db
-      .collection<Omit<User, "id">>("users")
+      .collection<MongoUser>("users")
       .findOne({ _id: new ObjectId(id) });
 
     if (!user) {
